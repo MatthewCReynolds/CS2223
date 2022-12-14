93950a7bad8abbdddaaaeecdb78cba1fc3172429
matthewreynolds <matthewreynolds@localhost>
2022-12-14 11:57:45 -0500

    Update #3, throwing null pointer exception in main block line 34

--- HW6/src/BoardSpot.java
+++ HW6/src/BoardSpot.java
@@ -10,7 +10,7 @@ public class BoardSpot {
 	}
 	
 	public boolean isAvailable() {
-		if((hasQueen == false) && (seenByQueenCount > 0))
+		if((hasQueen == false) && (seenByQueenCount == 0))
 			return true;
 		
 		return false;
--- HW6/src/Main.java
+++ HW6/src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 	
-	public boolean isLegalPosition(int[] givenBoard, int n){
+	public static boolean isLegalPosition(int[] givenBoard, int n){
 		boolean legal = false;
 		BoardSpot[][] board = new BoardSpot[n][n];
 		int actualPositions = givenBoard.length;
@@ -31,17 +31,17 @@ public class Main {
 		}
 		
 		for(int i = 0; i < actualPositions; i++) { //For every item in array thats not the last 0s
-			if(board[i][givenBoard[i]].seenByQueenCount == 0 && givenBoard[i] >= 0) { //if BoardSpot is !seen by queen and needs to be placed on this row
+			if(board[i][givenBoard[i]].isAvailable() && givenBoard[i] >= 0) { //if BoardSpot is !seen by queen and needs to be placed on this row
 				
 				board[i][givenBoard[i]].placeQueen();
 				
 				for(int j = 0; j < n; j++) {//Update all spots seen by queen on same row
-					if(!board[i][givenBoard[i]].hasQueen) {
+					if(!board[i][givenBoard[i]].isAvailable()) {
 						board[i][j].seen();
 					}
 				}
 				for(int j = 0; j < n; j++) {//Update all spots seen by queen on same column
-					if(!board[i][givenBoard[i]].hasQueen) {
+					if(!board[i][givenBoard[i]].isAvailable()) {
 						board[i][i].seen();
 					}
 				}
@@ -52,7 +52,7 @@ public class Main {
 					startCol--;
 				}
 				while(startRow > 0 && startRow < n && startCol > 0 && startCol < n) { //Update on positive slope diagonal
-					if(!board[i][givenBoard[i]].hasQueen) {
+					if(!board[i][givenBoard[i]].isAvailable()) {
 						board[startRow][startCol].seen();
 					}
 					startRow--;
@@ -89,7 +89,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Print");
+		int[] testBoard = {1,6,8,3,7,0,0,0};
+		int n = 8;
+		System.out.println(isLegalPosition(testBoard,n)); //should return true
 		
 	}
 
